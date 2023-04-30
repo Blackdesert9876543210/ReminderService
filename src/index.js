12ac5aae0f22d64e4d7a07475e967e3a59d9ee6d
AshimBarman <ashlabbarman@gmail.com>
2023-04-30 19:17:53 +0530

    Setup of smtp usingnodemailer done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const{ PORT } = require('./config/serverConfig');
 
+const { sendBasicEmail } = require('./services/email-service');
+
 const setUpAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
@@ -10,6 +12,13 @@ const setUpAndStartServer = () => {
 
     app.listen(PORT, () => {
        console.log(`server started at port ${PORT}`);
+
+       sendBasicEmail(
+        'airlinebookingsystem999@gmail.com',
+        'ashlabbarman@gmail.com',
+        'this is a funcking testing email',
+        'hello ji kaise ho saare this is love babbar'
+       );
     });
 }
 
